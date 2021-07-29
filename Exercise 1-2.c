ee673a261bbafe82b95c474093d1d8fdc6aef2f4
Ayan Acharya <ayan.acharya02@gmail.com>
2021-07-29 15:19:29 +0530

    Add files via upload

---
+++ Exercise 1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\\a Hello,\a World\n");
+    printf("\\b Hello,\b World\n");
+    printf("\\e Hello,\e World\n");
+    printf("\\f Hello,\f World\n");
+    printf("\\r Hello,\r World\n");
+    printf("\\t Hello,\t World\n");
+    printf("\\v Hello,\v World\n");
+}
